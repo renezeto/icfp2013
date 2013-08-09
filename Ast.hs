b174d2016ea767a99463a7c62e4d1657b11ef8b8
David Roundy <roundyd@physics.oregonstate.edu>
2013-08-08 20:46:11 -0700

    work on OperatorSet

--- Ast.hs
+++ Ast.hs
@@ -1,24 +1,15 @@
 module Ast where
 
-import Data.Word ( Word64 )
+import Data.Word ( Word16 )
+import Data.Bits ( Bits(..) )
 
 data Ast = Zero | One | X | Y | Z
-         | If0 Ast Ast Ast
-         | Fold Ast Ast Ast
-         | Not Ast
-         | Shl1 Ast
-         | Shr1 Ast
-         | Shr4 Ast
-         | Shr16 Ast
-         | And Ast Ast
-         | Or Ast Ast
-         | Xor Ast Ast
-         | Plus Ast Ast
+         | If0 Ast Ast Ast | Fold Ast Ast Ast
+         | Not Ast | Shl1 Ast | Shr1 Ast | Shr4 Ast | Shr16 Ast
+         | And Ast Ast | Or Ast Ast | Xor Ast Ast | Plus Ast Ast
          deriving ( Read, Show, Eq, Ord )
 
-data OperatorSet = Ifop | Foldop | Notop | Shl1op | Shr1op
-              | Shr4op | Shr16op | Andop | Orop | Xorop | Plusop
-
+data OperatorSet = OS Word16
 
 
 -- eval
@@ -37,8 +28,27 @@ data OperatorSet = Ifop | Foldop | Notop | Shl1op | Shr1op
 
 -- OperatorSet code:
 
+op_if = OS 1
+op_fold = OS 2
+op_not = OS 4
+op_shl1 = OS 8
+op_shr1 = OS 16
+op_shr4 = OS 32
+op_shr16 = OS 64
+op_and = OS 128
+op_or = OS 256
+op_xor = OS 512
+op_plus = OS 1024
+
 -- union
 
--- difference
+union :: OperatorSet -> OperatorSet -> OperatorSet
+union (OS a) (OS b) = OS (a .|. b)
+
+difference :: OperatorSet -> OperatorSet -> OperatorSet
+difference (OS a) (OS b) = OS (a .&. complement b)
+
+intersection :: OperatorSet -> OperatorSet -> OperatorSet
+intersection (OS a) (OS b) = OS (a `xor` b)
 
 -- add/remove, etc
