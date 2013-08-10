4a59e5bdd03b5acc34e2db963ae53b782bbc750a
renezeto <rene.zeto@gmail.com>
2013-08-09 20:02:38 -0700

    git sync problems

--- pull_from_everyone.sh
+++ pull_from_everyone.sh
@@ -2,8 +2,8 @@
 
 set -ev
 
-git pull quipu.physics.oregonstate.edu:/home/droundy/git/icfp2013.git
+git pull zetor@quipu.physics.oregonstate.edu:/home/droundy/git/icfp2013.git
 
-git pull quipu.physics.oregonstate.edu:/home/paho/git/icfp2013.git
+git pull zetor@quipu.physics.oregonstate.edu:/home/paho/git/icfp2013.git
 
-git pull quipu.physics.oregonstate.edu:/home/zetor/git/icfp2013.git
+git pull zetor@quipu.physics.oregonstate.edu:/home/zetor/git/icfp2013.git
