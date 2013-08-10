de2a08f1248b48b8661ae91860bf7a0ee48a6ef0
David Roundy <roundyd@physics.oregonstate.edu>
2013-08-10 06:27:04 -0700

    make get_training output a convenient commandline for timing that training

--- get_training.hs
+++ get_training.hs
@@ -57,4 +57,6 @@ main = do args <- getArgs
           let probsize = (read (head args))
           tr <- getTrain probsize
           putStrLn $ show tr
+          putStrLn $ "./build-all.sh && time ./make_guess time " ++ show (problemsize tr) ++
+            " " ++ problemid tr
           saveTrain tr
