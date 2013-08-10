b41217223dd9a62c8fcfd4bcf32174c7a0744d10
renezeto <rene.zeto@gmail.com>
2013-08-10 16:34:57 -0700

    solves real problems

--- submit.py
+++ submit.py
@@ -2,33 +2,6 @@ import os
 import sys
 import time
 import numpy as np
-#error:
-#response: Too many requests
-
-class OverLimit(Exception):
-    pass
-
-def commandCheck(f): #never passes raises error - bug
-    with open("status.txt","w") as create:
-        pass
-    def checkOutput(command):
-        os.system("%s > status.txt"%(command))
-        with open("status.txt","r") as openedLog:
-            print openedLog.readlines()
-            if "response: Too many requests\n" in openedLog.readlines():
-                raise OverLimit()
-    return checkOutput
-
-@commandCheck
-def runcmd(command):
-    return 0
-
-def foo():
-    for j in range(30):
-        if j==25:
-            raise OverLimit()
-        else:
-            runcmd("echo hello...")
 
 def main():
     if "get" in sys.argv:
@@ -36,19 +9,18 @@ def main():
         size = sys.argv[3]
         i = 0
         while i<n:
-            try:
-                runcmd("./get_training %s"%(size))
-                i+=1
-            except OverLimit:
-                print "Too many requests! Trying again..."
-                time.sleep(1)
-                i+=1
+            os.system("./get_training %s"%(size))
+            i+=1
     if "solve" in sys.argv:
+        problems_str = ""
         size = sys.argv[2]
         probIDs = [name for name in os.listdir('trainings/%s'%(size))]
+        if "problems" in sys.argv:
+            problems_str = "problem"
+            probIDs = [name for name in os.listdir('problems/%s'%(size))]
         for problem in probIDs:
-            runcmd("./make_guess %s %s"%(size, problem))
-
+            os.system("./make_guess %s %s %s"%(size, problem, problems_str))
+            time.sleep(10)
 
 #        ./make_guess 3 iwxYDVp0AsouTAvEEKBgwy1E
 if __name__ == '__main__':
